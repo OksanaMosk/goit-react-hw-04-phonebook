aa20c2d060470cde0df5c0eae1a63f54b05b66f0
OksanaMosk <oks-m3@ukr.net>
2023-10-31 00:25:31 +0200

    0

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,64 +3,69 @@
 import React from "react";
 import css from './ContactForm.module.css'
 import { nanoid } from 'nanoid';
+import { useState } from "react";
 
 
-
-class ContactForm extends React.Component {
-
-    state = {
-        name: '',
-        number: ''
-    };
-    
+export default function ContactForm () {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+ 
    
     
- handleSubmit = event => {
+ const handleSubmit = event => {
         event.preventDefault();
-
-        this.props.onSubmit({ name: this.state.name, number: this.state.number });
-        this.reset();
+//  useEffect(() => {
+//     reset({
+//      setName: [''],
+//        setNumber: [''],
+//     })
+//  }, [reset])
+     
+        this.props.onSubmit({ name: setName, number: setNumber });
+       
     };
+
+
     
-
-
- handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState ({ [name]: value });
+    const handleInputChange = event => {
+        const value = event.target.value;
+     setName: [] = event.target;
+     
     };
 
-    reset = () => {
-        this.setState({ number: '', name: '' });
-    };
+    // const reset = (() => {
+    //     setNumber: [''],
+    //     setName: [''],
+    // });
 
-    nameInputId = nanoid();
-    numberInputId = nanoid();
+    const nameInputId = nanoid();
+   const numberInputId = nanoid();
     
-    render() {
+    
         return (
             <div className={css.contactForm}>
-                <form className={css.form} onSubmit={this.handleSubmit}>
-                    <label htmlFor={this.nameInputId}>
+                <form className={css.form} onSubmit={handleSubmit}>
+                    <label htmlFor={nameInputId}>
                         <p className={css.inputName}>Name
                         </p>
                         <input type="text"
                             name="name"
-                            id={this.nameInputId}
-                            value={this.state.name}
-                            onChange={this.handleInputChange}
+                            id={nameInputId}
+                            value={name}
+                            onChange={handleInputChange}
                             className={css.formInput}
                             placeholder="Name"
                             required>
                         </input>
                     </label>
-                      <label htmlFor={this.numberInputId}>
+                      <label htmlFor={numberInputId}>
                         <p className={css.inputName}>Number
                         </p>
                         <input type="tel"
                             name="number"
-                            id={this.numberInputId}
-                            value={this.state.number}
-                            onChange={this.handleInputChange}
+                            id={numberInputId}
+                            value={number}
+                            onChange={handleInputChange}
                             className={css.formInput} 
                             pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
                             placeholder="000-00-00"
@@ -72,8 +77,8 @@ class ContactForm extends React.Component {
              </form>
             </div>
         )
-    }
+
 };
 
 
-export default ContactForm;
+
